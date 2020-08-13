e4b192e0a61b89c70033e7ed95c9f160c4f362fe
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-13 16:34:06 +0900

    [step11]spec書けた

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Home", type: :system, js: true do
   before do
-    binding.pry
+    current_at
     ancient_task
+    current_task
     future_task
     created_tasks
   end
@@ -12,22 +13,35 @@ RSpec.describe "Home", type: :system, js: true do
   let(:ancient_task) do
     create(
       :task,
-      title: (current_at - 1.day).strftime('%Y%m%d'),
-      updated_at: (current_at - 1.day).strftime('%Y%m%d')
+      title: (current_at - 1000.year).strftime('%Y%m%d'),
+      updated_at: (current_at - 1000.year).strftime('%Y%m%d')
     )
   end
-  let(:future_task) do
+  let(:current_task) do
     create(
       :task,
       title: current_at.strftime('%Y%m%d'),
       updated_at: current_at.strftime('%Y%m%d')
     )
   end
+  let(:future_task) do
+    create(
+      :task,
+      title: (current_at + 1000.year).strftime('%Y%m%d'),
+      updated_at: (current_at + 1000.year).strftime('%Y%m%d')
+    )
+  end
   let(:created_tasks) { Task.order(created_at: :desc) }
+
+  let(:titles) do
+    all('.task-title')
+  end
 
   describe "ホーム画面にアクセスしたとき" do
     it 'タスク一覧が表示されること' do
-        expect(page).to have_content(current_at.strftime('%Y%m%d'))
+        visit '/'
+        expect(titles[0].text).to have_content(future_task.updated_at.strftime('%Y%m%d'))
+        expect(titles[2].text).to have_content(ancient_task.updated_at.strftime('%Y%m%d'))
     end
   end
 end
