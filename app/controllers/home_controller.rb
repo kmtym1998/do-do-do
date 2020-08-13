5b92d82727b73546434e967f30b5cb2c7a51939b
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-13 18:42:04 +0900

    [step11]テストDB以外のデータも消えちゃう問題対策

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def top
+    if User.count == 0 && Category.count == 0
+      User.create(id: 1, name: 'user1', password: 'sample')
+      Category.create(id: 1, title: 'categoryr1', user_id: 1)
+    end
+
+    if Task.count == 0
+      Task.create(title: 'データ消えすぎ問題対策', deadline: Time.zone.now, priority: rand(0..2), state: rand(0..2), memo: 'test', user_id: 1, category_id: 1)
+    end
+    
     # 日付の場合、昇順→古い順　降順→新しい順
     @tasks = Task.order(created_at: :desc)
     # @tasks = Task.order(created_at: 'DESC')質問
