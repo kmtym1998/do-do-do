f9b17c4c58963b0356eb15571b526469ff2ebdc6
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-15 22:09:54 +0900

    [step12]モデルにもバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true, length: {maximum: 30}
+  validates :user_id, presence: true
+  validates :category_id, presence: true
 end
