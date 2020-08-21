7112c94ddfc87d5129f2b4fb40c7b771cdac6a94
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-21 11:35:58 +0900

    [step21]create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.delete_all
+Category.delete_all
+User.delete_all
+
+User.create({password: "sample_pass", name: "sample_auth_user", sort_state: 0, admin: true})
+User.create({password: "sample_pass", name: "sample_auth_user", sort_state: 0})
+Category.create({title: "sample_category1", user_id: User.first.id})
+Category.create({title: "sample_category2", user_id: User.first.id})
+Task.create({title: "sample_task1", deadline: Time.current + 1.hour, user_id: User.first.id, category_id: Category.first.id})
+Task.create({title: "sample_task2", deadline: Time.current + 1.hour, user_id: User.first.id, category_id: Category.first.id})
