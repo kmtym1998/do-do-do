923c13f62c87e66588fb0a57599615212c584673
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-21 18:42:30 +0900

    [step22]ログイン・ログアウトのロジック記述

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,34 +1,58 @@
 class HomeController < ApplicationController
   def top
-    if User.count == 0 && Category.count == 0
-      User.create(id: 1, name: 'user1', password: 'sample', sort_state: 0)
-      Category.create(id: 1, title: 'categoryr1', user_id: 1)
+    redirect_to(login_path) if !session[:login]
+
+    if Category.where(user_id: session[:login]).count == 0
+      Category.create(title: "sample", user_id: session[:login])
     end
 
-    if Task.count == 0
-      Task.create(title: 'データ消えすぎ問題対策', deadline: Time.zone.now, priority: rand(0..2), state: rand(0..2), memo: 'test', user_id: 1, category_id: 1)
-    end
+    @sort_state = User.find(session[:login]).sort_state
 
-    @sort_state = User.first.sort_state
+    order = case @sort_state
+              when 0 then
+                {deadline: :desc}
+              when 1 then
+                {created_at: :desc}
+              when 2 then
+                {updated_at: :desc}
+            end
 
-    case @sort_state
-    when 0
-      @tasks = Task.order(deadline: :desc)
-    when 1
-      @tasks = Task.order(created_at: :desc)
-    when 2
-      @tasks = Task.order(updated_at: :desc)
-    else
-    end
+    @tasks = Task.where(user_id: session[:login]).order(order)
   end
 
   def sort_tasks
-    @user = User.first
+    @user = User.find(session[:login])
     @user.sort_state = params[:sort_state]
     @user.save
   end
   
 
   def login
+    redirect_to(root_path) if session[:login].present?
   end
+
+  def auth    
+    id = params[:id]
+    user = User.find_by(id: id)
+
+    if user
+      if user.authenticate(params[:password])
+        session[:message] = ""
+        session[:login] = id
+        redirect_to(root_path)
+      else
+        session[:message] = "IDは見つかったが、パスワードが違う"
+        redirect_to(login_path)
+      end
+    else
+      session[:message] = "IDが見つからない"
+      redirect_to(login_path)
+    end
+  end
+
+  def logout
+    session[:login] = nil
+    redirect_to(login_path)
+  end
+  
 end
