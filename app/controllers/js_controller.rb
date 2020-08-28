bd9ea1f1f6502007c122f5ae8b9f94e516f06dcc
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-29 02:26:07 +0900

    [step25]カテゴリの作成・削除・更新処理の実装

--- app/controllers/js_controller.rb
+++ app/controllers/js_controller.rb
@@ -1,23 +1,32 @@
 class JsController < ApplicationController
   def ajax_create
-    @title = params[:newTaskTitle]
     @message = ''
-
-    @newTask = Task.create(
-      title: @title,
-      deadline: Time.zone.now + 1.day,
-      priority: rand(0..2),
-      state: rand(0..2),
-      memo: 'test',
-      user_id: session[:login],
-      category_id: Category.where(user_id: session[:login]).first.id
-    )
-    if @newTask.valid?
-      @id = @newTask.id
-    else
-      @message = @newTask.errors.full_messages
+    if params[:newTaskTitle]
+      @newTask = Task.create(
+        title: params[:newTaskTitle],
+        deadline: Time.zone.now + 1.day,
+        priority: rand(0..2),
+        state: rand(0..2),
+        memo: 'test',
+        user_id: session[:login],
+        category_id: params[:category_id]
+      )
+      if @newTask.valid?
+        @id = @newTask.id
+        redirect_to("#{request.protocol}#{request.host}/category/#{params[:category_id]}")
+      else
+        @message = @newTask.errors.full_messages
+      end
+    elsif params[:newCategoryTitle]
+      @newCategory = Category.create(
+        title: params[:newCategoryTitle],
+        user_id: session[:login]
+      )
+      unless @newCategory.valid?
+        @message = @newTask.errors.full_messages
+      end
+      redirect_to(root_path)
     end
-    render
   end
 
   def ajax_before_edit
@@ -28,27 +37,42 @@ class JsController < ApplicationController
   def ajax_edit
     @message = ''
 
-    @task = Task.find(params[:task_id])
-    @task.title = params[:title]
-    @task.deadline = params[:deadline]
-    @task.priority = params[:priority]
-    @task.state = params[:state]
-    @task.memo = params[:memo]
-    @task.save
-
-    if !@task.valid?
-      @message = @task.errors.full_messages
+    if params[:task_id].present?
+      @task = Task.find(params[:task_id])
+      @task.title = params[:title]
+      @task.deadline = params[:deadline]
+      @task.priority = params[:priority]
+      @task.state = params[:state]
+      @task.memo = params[:memo]
+      @task.save
+      unless @task.valid?
+        @message = @task.errors.full_messages
+      end
+    elsif params[:category_id].present?
+      @category = Category.find(params[:category_id])
+      @category.title = params[:category_title]
+      @category.save
+      unless @category.valid?
+        @message = category.errors.full_messages
+      end
     end
+    
+
     
     render
   end
 
   def ajax_delete
-    @task_id = params[:task_id]
-    @task = Task.find(@task_id)
-    @task.destroy
-
-    render
+    if params[:task_id].present?
+      @id = params[:task_id]
+      task = Task.find(@id)
+      task.destroy
+    elsif params[:category_id].present?
+      @id = params[:category_id]
+      category = Category.eager_load(:tasks).find(@id)
+      category.destroy
+      redirect_to(root_path)
+    end
   end
 
 end
