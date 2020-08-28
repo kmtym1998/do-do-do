56a30adad234a9a10815972700e096f2823fe6e6
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-28 13:00:58 +0900

    [step24]管理者が0人にならないようモデルのコールバックを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ApplicationRecord
+    after_destroy :check_if_admin_exists
+    after_save :check_if_admin_exists
+
     has_many :tasks, dependent: :destroy
     has_many :categories, dependent: :destroy
     has_secure_password
 
     validates :name, presence: true, length: {maximum: 30}
     validates :password, presence: true, length: {maximum: 30}
+
+    def check_if_admin_exists
+        if User.where(is_admin: true).count.zero?
+            raise StandardError, "管理者を0人にすることはできません"
+        end
+    end
+    
 end
