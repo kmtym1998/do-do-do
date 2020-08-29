28a085defbdffb1831f47adcc873ea7d40c7b13d
小松山凌平 <r.komatsuyama@buysell-technologies.com>
2020-08-29 10:48:54 +0900

    [bugfix]本番環境で動かない不具合修正

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Bullet.enable = true
-Bullet.alert = true
-Bullet.bullet_logger = true
-Bullet.console = true
-Bullet.rails_logger = true
-Bullet.add_footer = true
